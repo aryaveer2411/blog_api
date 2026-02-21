b655df237e3dcc4ed0040f28c4e4652ab5b04f97
Aryaveer Chaudhary <aryaveerchaudhary007@gmail.com>
2026-02-22 01:58:35 +0530

    fix: changed style of asynchandler for better understanding

--- src/utils/async_handler.ts
+++ src/utils/async_handler.ts
@@ -1,12 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 
 type IAsyncHandler = (req: Request, res: Response, next: any) => Promise<any>;
-export const asyncHandler =
-  (fnc: IAsyncHandler) =>
-  async (req: Request, res: Response, next: NextFunction) => {
+
+export const asyncHandler = (fnc: IAsyncHandler) => {
+  const wrapper = async (req: Request, res: Response, next: NextFunction) => {
     try {
       await fnc(req, res, next);
     } catch (e) {
       next(e);
     }
   };
+  return wrapper;
+};
