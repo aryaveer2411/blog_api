a6530ca2a69cc0a6c1c2624bd706995fefb58a0a
Aryaveer Chaudhary <aryaveerchaudhary007@gmail.com>
2026-03-21 02:39:23 +0530

    fix: fix types for winston

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,11 +5,17 @@ const logger = winston.createLogger({
   format: winston.format.combine(
     winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     winston.format.errors({ stack: true }),
-    winston.format.printf(({ timestamp, level, message, stack }) =>
-      stack
+    winston.format.printf((info) => {
+      const { timestamp, level, message, stack } = info as {
+        timestamp: string;
+        level: string;
+        message: string;
+        stack?: string;
+      };
+      return stack
         ? `${timestamp} [${level.toUpperCase()}]: ${message}\n${stack}`
-        : `${timestamp} [${level.toUpperCase()}]: ${message}`,
-    ),
+        : `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    }),
   ),
   transports: [new winston.transports.Console()],
 });
